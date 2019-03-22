606fc91cfd16075fd253d4400d00610170c2a70f
DominikBoschert <48676499+DominikBoschert@users.noreply.github.com>
2019-03-22 12:27:03 +0100

    Add files via upload

---
+++ h-bruecke/h-bruecke.ino
@@ -0,0 +1,48 @@
+// Gleichstrommotor 1
+
+int GSM1 = 10;
+int in1 = 9;
+int in2 = 8;
+
+// Gleichstrommotor 1
+
+int GSM2 = 5;
+int in3 = 7;
+int in4 = 6;
+
+void setup()
+{
+  Serial.begin(9600);
+  //pinMode(GSM1, OUTPUT);    
+  //pinMode(GSM2, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+}
+void loop()
+{
+  digitalWrite(in1, HIGH);  // Motor 1 beginnt zu rotieren
+  digitalWrite(in2, LOW);
+  Serial.println("1-2");
+ // analogWrite(GSM1, 200);   // Motor 1 soll mit der Geschwindigkeit "200" (max. 255) rotieren 
+
+  digitalWrite(in3, HIGH);  // Motor 2 beginnt zu rotieren
+  digitalWrite(in4, LOW);
+  Serial.println("3-4");
+  //analogWrite(GSM2, 200);   // Motor 2 soll ebenfalls mit der Geschwindigkeit "200" (max. 255) rotieren
+  delay(2000);
+
+  digitalWrite(in1, LOW);   // Durch die Veränderung von HIGH auf LOW (bzw. LOW auf HIGH) wird die Richtung der Rotation verändert.
+  digitalWrite(in2, HIGH);  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH); 
+  delay(2000);
+
+  digitalWrite(in1, LOW);   // Anschließend sollen die Motoren 2 Sekunden ruhen.
+  digitalWrite(in2, LOW);  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+
+  delay(2000);
+}
---
+++ sketch_mar22a/sketch_mar22a.ino
@@ -0,0 +1,28 @@
+
+byte byteArray[16]{
+  0x03, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00,
+  0x00, 0x00, 0x00, 0x00,
+};
+unsigned long number = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  DumpByteArrayAsHex(byteArray, 16);
+  Serial.println();
+  number = (unsigned long) *byteArray;
+  Serial.println(number);
+
+}
+
+void DumpByteArrayAsHex(byte *buffer, byte bufferSize) {
+  for(byte i = 0; i < bufferSize; i++){
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
